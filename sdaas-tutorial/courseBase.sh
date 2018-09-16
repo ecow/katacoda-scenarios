de4c8e4c6fb5977e5578704754d50c7e7c86a2d8
admin@e-artspace.com <enrico@linkeddata.center>
2018-09-16 23:10:51 +0200

    test

--- sdaas-tutorial/courseBase.sh
+++ sdaas-tutorial/courseBase.sh
@@ -1,3 +1,5 @@
-ssh root@host01 "docker pull linkeddatacenter/sdaas-ce:latest"
-ssh root@host01 "docker run --name sdaas -d -p 8889:8080 -v PWD/.:/workspace linkeddatacenter/sdaas-ce"
-ssh root@host01 "docker exec sddas bash"
+echo "Installing SDaaS platform..."
+ssh root@host01 "docker pull linkeddatacenter/sdaas-ce:latest" > /dev/null
+ssh root@host01 "docker run --name sdaas -d -p 8889:8080 -v /root:/workspace linkeddatacenter/sdaas-ce" > /dev/null
+ssh root@host01 -t "docker exec -ti sdaas bash"
+
